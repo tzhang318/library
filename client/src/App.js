83df109877da786a8181dea5d42eeabf417a7ca7
Tim Zhang <zhang.tim@gmail.com>
2023-05-01 14:03:17 -0500

    first crack at image carasel

--- client/src/App.js
+++ client/src/App.js
@@ -25,14 +25,16 @@ const App = () => {
 
   return (
     <div>
-      <div style={{ marginBottom: '2rem' }}>
-        <Link to='/authors' style={{ paddingRight: '1rem'}}>authors</Link>
-        <Link to='/books' style={{ paddingRight: '1rem'}}>books</Link>
-        {!token && <Link to='/login' style={{ paddingRight: '1rem'}}>login</Link>}
-        {token && <Link to='add' style={{ paddingRight: '1rem'}}>add book</Link>}
-        {token && <Link to='recommend'>recommend</Link>}
-        {token && <button style={{ position: 'absolute', right: '5rem' }} onClick={logout}>logout</button>}
-        <Link to='/tic' >Game</Link>
+      <div className='menu'>
+        <div className='nav'>
+          <Link className='link' to='/authors' style={{ paddingRight: '1rem'}}>authors</Link>
+          <Link className='link' to='/books' style={{ paddingRight: '1rem'}}>books</Link>
+          {!token && <Link className='link' to='/login' style={{ paddingRight: '1rem'}}>login</Link>}
+          {token && <Link className='link' to='add' style={{ paddingRight: '1rem'}}>add book</Link>}
+          {token && <Link className='link' to='recommend'>recommend</Link>}
+          <Link className='link' to='/tic' >Game</Link>
+        </div>
+        {token && <button className='logout'  onClick={logout}>logout</button>}
       </div>
 
       <ToastContainer autoClose={8000} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,14 @@
 import { useState, useEffect } from 'react';
+import barcelona from '../assets/barcelona.svg';
+import rm from '../assets/real-madrid.svg';
+import atm from '../assets/athletic-madrid.svg';
+import sevilla from '../assets/sevilla.svg';
+import bilbao from '../assets/bilbao.svg';
+import sociadad from '../assets/real-sociedad.svg';
+import betis from '../assets/real-betis.svg';
+import valencia from '../assets/valencia.svg';
+import espanyol from '../assets/espanyol.svg';
+import villarreal from '../assets/villarreal.svg';
 
 import '../App.css';
 
@@ -20,20 +30,53 @@ export const Home = props => {
   }, [percent]);
 
   return (
-    <div style={{ marginTop: '5rem', fontWeight: 'bold', textAlign: 'center' }} >
-      Welcome to our library
+    <div style={{ marginTop: '2rem', fontWeight: 'bold', textAlign: 'center' }} >
+      <h3>Welcome to our library</h3>
+      <div className='logo-slider'>
+        <div className='logos'>
+          <div>
+            <img src={barcelona} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={rm} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={atm} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={sevilla} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={bilbao} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={valencia} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={sociadad} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={betis} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={espanyol} className='logo' alt='bfc' />
+          </div>
+          <div>
+            <img src={villarreal} className='logo' alt='bfc' />
+          </div>
+        </div>
+      </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ marginRight: '2rem' }}>
-        <h3>Pie</h3>
-        <div className="pie" style={{ "--percentage":`${percent}%`}}>
+        <div style={{ marginRight: '2rem' }}>
+          <h3>Pie</h3>
+          <div className="pie" style={{ "--percentage":`${percent}%`}} />
         </div>
-      </div>
-      <div>
-        <h3>Ring</h3>
-        <div className="pie" style={{ "--percentage":`${percent}%`}}>
-          <div className='center'></div>
+        <div>
+          <h3>Ring</h3>
+          <div className="pie" style={{ "--percentage":`${percent}%`}}>
+            <div className='center'></div>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
